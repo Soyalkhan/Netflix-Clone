8418c70043049beb18aa42fd4d940ff40470f5aa
soyalkhan <soyalkhan76@gmail.com>
2024-03-13 23:20:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// let table = prompt("Enter table you want to print:")
+// for (i = 1; i <= 10; i++) {
+//     console.log(table + " x " + i + " = " + i * table);
+// }
+
+
+// let a = prompt("enter first no");
+// a = parseInt(a)
+// let b = prompt("enter second no");
+// b = parseInt(b)
+// let sum = a + b;
+// alert("sum of number is :" + sum)
+
